a98ee4cebac122b9193977eff63cd5453d7f797c
MarioGL <72580321+magarclaf@users.noreply.github.com>
2023-12-03 02:50:21 +0100

    Comienzo de PartidaMultijugador.java

--- UltimateRoshambo/src/roshambo/PartidaMultijugador.java
+++ UltimateRoshambo/src/roshambo/PartidaMultijugador.java
@@ -1,5 +1,17 @@
 package roshambo;
 
-public class PartidaMultijugador {
+import java.net.Socket;
+
+import javax.swing.JFrame;
+
+public class PartidaMultijugador extends JFrame{
+
+	private Socket s;
+	private String nombre;
+	private int seleccion;
+	
+	public PartidaMultijugador(Socket s, String nombre, int seleccion) {
+		// TODO Auto-generated constructor stub
+	}
 
 }
